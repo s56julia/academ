1e869ccc56007dd49100c830a24d69d2b3e6c43d
Julia Khrysieva <s56julia@gmail.com>
2021-11-03 21:07:43 +0200

    Homework1 added ApiController and test for it

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+#[Route('/api', name: 'api_')]
+class ApiController
+{
+    #[Route('/', name: 'index')]
+    public function index(UrlGeneratorInterface $urlGenerator, Request $request): Response
+    {
+        $this->checkContentType($request);
+
+        $urls = [];
+        $urls[] = $urlGenerator->generate('api_index', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        $urls[] = $urlGenerator->generate('api_my_info', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        foreach (array_keys($this->getProductsData()) as $sku) {
+            $urls[] = $urlGenerator->generate('api_view', ['sku' => $sku], UrlGeneratorInterface::ABSOLUTE_URL);
+        }
+
+        return new JsonResponse($urls);
+    }
+
+    #[Route('/my-info', name: 'my_info', priority: 10)]
+    public function info(Request $request): Response
+    {
+        $this->checkContentType($request);
+
+        $ua = $request->server->get('HTTP_USER_AGENT');
+        $lang = $request->server->get('HTTP_ACCEPT_LANGUAGE');
+        $ip = $request->getClientIp();
+
+        $response = new JsonResponse(['browser' => $ua, 'ip' => $ip, 'lang' => $lang]);
+        $logDir = realpath($this->getVarDirPath().\DIRECTORY_SEPARATOR.'log');
+        file_put_contents($logDir.\DIRECTORY_SEPARATOR.'my-info.log', $response->getContent().\PHP_EOL, \FILE_APPEND);
+
+        return $response;
+    }
+
+    #[Route('/{sku}', name: 'view')]
+    public function view(string $sku, Request $request): Response
+    {
+        $this->checkContentType($request);
+
+        $products = $this->getProductsData();
+        if (!\array_key_exists($sku, $products)) {
+            throw new NotFoundHttpException('Unknown sku requested');
+        }
+
+        return new JsonResponse($products[$sku]);
+    }
+
+    protected function getVarDirPath(): string
+    {
+        return realpath(__DIR__.'/../../var');
+    }
+
+    protected function getProductsData(): array
+    {
+        $logDir = $this->getVarDirPath().\DIRECTORY_SEPARATOR.'data';
+        $handle = fopen($logDir.\DIRECTORY_SEPARATOR.'products.csv', 'r');
+        $headerRow = fgetcsv($handle);
+
+        $rows = [];
+        while (($data = fgetcsv($handle)) !== false) {
+            $row = array_combine($headerRow, $data);
+            $rows[$row['sku']] = $row;
+        }
+        fclose($handle);
+
+        return $rows;
+    }
+
+    protected function checkContentType(Request $request): void
+    {
+        if ('application/json' !== $request->headers->get('Content-Type')) {
+            throw new UnsupportedMediaTypeHttpException();
+        }
+    }
+}
---
+++ tests/Controller/ApiControllerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\Router;
+
+/**
+ * Functional test for the controllers defined inside ApiControllerTest.
+ *
+ * See https://symfony.com/doc/current/testing.html#functional-tests
+ *
+ * Execute the application tests using this command (requires PHPUnit to be installed):
+ *
+ *     $ cd your-symfony-project/
+ *     $ ./vendor/bin/phpunit
+ */
+class ApiControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->jsonRequest('GET', '/api/');
+
+        $this->assertResponseIsSuccessful();
+
+        $urls = json_decode($client->getResponse()->getContent());
+        $this->assertCount(12, $urls);
+
+        /** @var Router $router */
+        $router = $this->getContainer()->get('router');
+        $this->assertContains($router->generate('api_index', [], UrlGeneratorInterface::ABSOLUTE_URL), $urls);
+        $this->assertContains($router->generate('api_my_info', [], UrlGeneratorInterface::ABSOLUTE_URL), $urls);
+        $this->assertContains($router->generate('api_view', ['sku' => 'product_001'], UrlGeneratorInterface::ABSOLUTE_URL), $urls);
+    }
+
+    public function testIndexNonJsonRequest(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/');
+
+        $this->assertResponseStatusCodeSame(415);
+    }
+
+    public function testMyInfo(): void
+    {
+        $client = static::createClient();
+
+        $dir = $this->getContainer()->getParameter('kernel.project_dir');
+        @unlink($dir.'/var/log/my-info.log');
+
+        $client->jsonRequest('GET', '/api/my-info');
+
+        $this->assertResponseIsSuccessful();
+
+        $data = json_decode($client->getResponse()->getContent(), \JSON_OBJECT_AS_ARRAY);
+
+        $this->assertArrayHasKey('browser', $data);
+        $this->assertNotEmpty($data['browser']);
+
+        $this->assertArrayHasKey('ip', $data);
+        $this->assertNotEmpty($data['ip']);
+
+        $this->assertArrayHasKey('lang', $data);
+        $this->assertNotEmpty($data['lang']);
+
+        $this->assertFileExists($dir.'/var/log/test.log');
+    }
+
+    public function testProduct(): void
+    {
+        $client = static::createClient();
+        $client->jsonRequest('GET', '/api/product_001');
+        $this->assertResponseIsSuccessful();
+
+        $data = json_decode($client->getResponse()->getContent(), \JSON_OBJECT_AS_ARRAY);
+        $this->assertArrayHasKey('sku', $data);
+        $this->assertSame('product_001', $data['sku']);
+        $this->assertArrayHasKey('name', $data);
+        $this->assertSame('apple', $data['name']);
+        $this->assertArrayHasKey('description', $data);
+        $this->assertSame('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do', $data['description']);
+    }
+
+    public function testProductNotEmpty(): void
+    {
+        $client = static::createClient();
+        $client->jsonRequest('GET', '/api/product_0111');
+        $this->assertResponseStatusCodeSame(404);
+    }
+}
